008c2be8390c2f56fffebb285762c06eab0e575c
hdhdh4226ru <snovamgodam55@gmail.com>
2025-05-08 23:37:01 +0300

    -8.05 24:36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
